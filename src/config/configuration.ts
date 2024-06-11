cdf6e3c847bf9fabc0b7bc252d62c0e43076057c
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-06-12 02:00:17 +0300

    configured config files, refactored app.module

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,17 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 4000,
-  db_host: process.env.POSTGRES_HOST,
-  db_port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
-  db_name: process.env.POSTGRES_DB,
-  db_user: process.env.POSTGRES_USER,
-  db_pass: process.env.POSTGRES_PASSWORD,
-  jwt_secret: process.env.PRIVATE_KEY,
-  synchronize: process.env.SYNCHRONIZE,
+  server: {
+    port: parseInt(process.env.PORT, 10) || 4000,
+  },
+  database: {
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
+    user: process.env.DB_USER || 'student',
+    password: process.env.DB_PASSWORD || 'student',
+    name: process.env.DB_NAME, // Название базы данных
+    synchronize: process.env.SYNCHRONIZE,
+  },
+  jwt: {
+    secretKey: process.env.JWT_SECRET,
+    tokenTimeLimit: process.env.TOKEN_TIME_LIMIT,
+  },
 });
